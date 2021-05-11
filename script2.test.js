1b30c79760839c460f8f7ab6b70a795ef77eb776
CatSheiles <cathysheiles@gmail.com>
2021-05-11 18:29:15 +1000

    initial

---
+++ script2.test.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+const swapi = require('./script2');
+
+it('calls swapi to get people', () => {
+    expect.assertions(1)
+    return swapi.getPeople(fetch).then(data => { //the return makes certain test is completed before passing or rejecting
+        expect(data.count).toEqual(87)
+    })
+})
+
+it('calls swapi to get people with a promise', () => {
+    expect.assertions(2)
+    return swapi.getPeoplePromise(fetch).then(data => {
+        expect(data.count).toEqual(87)
+        expect(data.results.length).toBeGreaterThan(5)
+    })
+})
+
+it('getPeople returns count and results', () => {
+    //mock test example using dependency injection instead of using up all resources in large apps.
+    const mockFetch = jest.fn()
+        .mockReturnValue(Promise.resolve({
+            json: () => Promise.resolve({
+                count: 87,
+                retults: [0,1,2,3,4,5]
+            })
+        }))
+    expect.assertions(4)
+    return swapi.getPeoplePromise(mockFetch).then(data => {
+        expect(mockFetch.mock.calls.length).toBe(1);
+        expect(mockFetch).toBeCalledWith('https://swapi.py4e.com/api/people')
+        expect(data.count).toEqual(87)
+        expect(data.results.length).toBeGreaterThan(10)
+    })
+})
